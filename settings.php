6bd648cbbc9e49fa3ac82861b8e281ca8a05f5c2
avi.megladon <avi.megladon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-14 04:46:52 +0000

    update..
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-with-shortcode/tags/1.1.1@989320 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -8,6 +8,7 @@ class contact_settings {
 	
 	function  contact_widget_afo_options () {
 	global $wpdb;
+	$this->contact_wid_pro_add();
 	?>
 	<table width="100%" border="0">
 	  <tr>
@@ -58,6 +59,15 @@ class contact_settings {
 	  
 	</table>
 	<?php }
+	
+	function contact_wid_pro_add(){ ?>
+	<table width="98%" border="0" style="background-color:#FFFFD2; border:1px solid #E6DB55; padding:0px 0px 0px 10px; margin:2px;">
+  <tr>
+    <td><p>The PRO version of this plugin supports <strong>Newsletter Subscription</strong> with option to choose different newsletter <strong>Themes</strong>. You can get it <a href="http://aviplugins.com/contact-form-with-shortcode-pro/" target="_blank">here</a> in <strong>USD 2.00</strong></p></td>
+  </tr>
+</table>
+	<?php }
+	
 	
 	function contact_widget_afo_menu () {
 		add_menu_page( 'Contact Form Usage', 'Contact Form Usage', 10, 'contact_form_settings', array( $this,'contact_widget_afo_options' ) );	

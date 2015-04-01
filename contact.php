7270ff1a0da5b1ff1fd92f22f9f98b4b6dee58c0
avi.megladon <avi.megladon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 16:42:33 +0000

    dgsdgasdg
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-with-shortcode/tags/2.1.1@1125567 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ Plugin Name: Contact Form With Shortcode
 Plugin URI: http://aviplugins.com/
 Description: This is a contact form plugin. You can use widgets and shortcodes to display contact form in your theme. Unlimited number of dynamic fields can me created for contact froms.
 Version: 2.1.1
-Author: aviplugins.com
+Author: avimegladon
 Author URI: http://avifoujdar.wordpress.com/
 */
 

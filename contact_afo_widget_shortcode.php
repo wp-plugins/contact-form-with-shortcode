bb8fdf1f89c10fd852143c449bb47fe4a889e546
avi.megladon <avi.megladon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-24 17:29:15 +0000

    upload
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-with-shortcode/tags/2.2.2@1187058 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ contact_afo_widget_shortcode.php
@@ -0,0 +1,24 @@
+<?php
+function contact_widget_shortcode( $atts ) {
+     global $post;
+	 extract( shortcode_atts( array(
+	      'title' => '',
+		  'id' => '',
+		  'ajax' => 'No'
+     ), $atts ) );
+    
+	if(!$id)
+	return;
+	
+	ob_start();
+	$cfw = new contact_form_wid;
+	if($title){
+		echo '<h2>'.$title.'</h2>';
+	}
+	$cfw->contactWidBody(array('wid_contact_form' => $id, 'wid_contact_ajax' => $ajax ));
+	$ret = ob_get_contents();	
+	ob_end_clean();
+	return $ret;
+}
+add_shortcode( 'contactwid', 'contact_widget_shortcode' );
+?>
